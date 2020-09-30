df643eddfa4b1f15e9fcacf2e093f87300161d11
dawitjabessa <dawitjabessa@cvpcorp.com>
2020-09-30 12:47:44 -0400

    Make updates to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,16 +4,20 @@ const request = require('request')
 const forcast = (a, b, callback) => {
  const url = 'http://api.weatherstack.com/current?access_key=425df3a9fbb238ac9e65519d57424adf&query='+b+ ',' +a+'&units=f'
     request({ url, json: true }, (error, { body}) => {
+
         if (error) {
             callback('Unable to connect to the weather service')
         } else if (body.error) {
             callback('unable to find location')
         } else {
+            
             callback(undefined, body.current.weather_descriptions[0] +
                 '. it is currently '
                 + body.current.temperature +
                  ' degrees out. it feels like ' + body.current.feelslike + 
-                ' degrees out')
+                ' degrees out' + '. The humidity is ' + body.current.humidity + '%')
+
+            
         }
     })
 }
